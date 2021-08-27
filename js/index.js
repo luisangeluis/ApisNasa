ce36934785b6b6a46395e94ac6353141decebed5
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-08-27 14:38:56 -0500

    Seguir la api de epic para ver que mas datos puede dar.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const miImagen = document.querySelector('#api-imgday');
 const infoParagraph = document.querySelector('#info-imgday');
 const titleDayPhoto = document.querySelector('#title-foto');
 
+
+//Api imagen del dia
 async function apiRequest() {
 
     try {
@@ -20,7 +22,7 @@ apiRequest()
         // console.log(response);
         return response.ok ? response.json() : Promise.reject(response);
     }).then(json => {
-        console.log(json);
+        // console.log(json);
         miImagen.src = json.url
         infoParagraph.textContent = json.explanation;
         titleDayPhoto.textContent = json.title;
@@ -33,60 +35,84 @@ apiRequest()
 
 //Api fotos rover de marte
 
-const divPhotosRover = document.querySelector('#photos-rover');
-const neoFeed = async() => {
+// const divPhotosRover = document.querySelector('#photos-rover');
+// const neoFeed = async() => {
 
+//     try {
+//         let data = await fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=Ah62SEfDVY3K4OiuUs4ZI33Honwahn3xtef48Ncm");
+//         return data;
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// }
+
+// neoFeed()
+//     .then(response => {
+//         //  console.log(response);
+//         return response.ok ? response.json() : Promise.reject(response);
+//     })
+//     .then(json => {
+//         // console.log(json)
+
+//         const fragmentPhotosRover = document.createDocumentFragment();
+
+//         json.photos.forEach(element => {
+//             const img = document.createElement('img');
+//             const div = document.createElement('div');
+
+//             img.setAttribute('class', 'rovert-img');
+//             img.src = element.img_src;
+//             div.appendChild(img);
+//             fragmentPhotosRover.appendChild(div);
+//             // console.log(element);
+//         });
+//         divPhotosRover.appendChild(fragmentPhotosRover);
+
+//         // const fragment = document.createDocumentFragment();
+
+//         // // json.forEach(element => {
+//         // //     const img = document.createElement('img');
+//         // //     let i = 0;
+
+
+
+//         // //     i++;
+
+//         // // });
+
+//         // for(let i =0; i<json.length; i++){
+//         //     const img = document.createElement('img');
+//         //     img.src = json[i].image+'.png';
+//         //     fragment.appendChild(img);
+
+//         // }
+
+//         // divEpic.appendChild(fragment);
+
+//     })
+//     .catch(error => console.log(`miError ${error}`))
+
+//Api epic
+
+const apiEpic = async() => {
     try {
-        let data = await fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=Ah62SEfDVY3K4OiuUs4ZI33Honwahn3xtef48Ncm");
-        return data;
+        let data = await fetch('https://api.nasa.gov/EPIC/api/natural/images?api_key=Ah62SEfDVY3K4OiuUs4ZI33Honwahn3xtef48Ncm');
 
+        return data;
     } catch (error) {
-        console.log(error);
+        console.log(`miError ${error}`);
     }
 
 }
-neoFeed()
-    .then(response => {
-        //  console.log(response);
-        return response.ok ? response.json() : Promise.reject(response);
+
+apiEpic()
+    .then(res => {
+        console.log(res);
+        return res.ok ? res.json() : Promise.reject(res);
     })
-    .then(json => {
-        // console.log(json)
-
-        const fragmentPhotosRover = document.createDocumentFragment();
-
-        json.photos.forEach(element => {
-            const img = document.createElement('img');
-            const div = document.createElement('div');
-
-            img.setAttribute('class', 'rovert-img');
-            img.src = element.img_src;
-            div.appendChild(img);
-            fragmentPhotosRover.appendChild(div);
-            // console.log(element);
-        });
-        divPhotosRover.appendChild(fragmentPhotosRover);
-
-        // const fragment = document.createDocumentFragment();
-
-        // // json.forEach(element => {
-        // //     const img = document.createElement('img');
-        // //     let i = 0;
-
-
-
-        // //     i++;
-
-        // // });
-
-        // for(let i =0; i<json.length; i++){
-        //     const img = document.createElement('img');
-        //     img.src = json[i].image+'.png';
-        //     fragment.appendChild(img);
-
-        // }
-
-        // divEpic.appendChild(fragment);
-
+    .then(res => {
+        console.log(res);
+        document.querySelector('#img-epic').src = `https://epic.gsfc.nasa.gov/archive/natural/2021/08/26/png/${res[0].image}.png`;
     })
-    .catch(error => console.log(`ERROR ${error}`))
